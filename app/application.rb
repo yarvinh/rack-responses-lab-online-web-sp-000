bb0b9f8ead0e7e3e57cbccd245cd79f42d1ff7b3
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-09 22:28:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
   resp = Rack::Response.new
   # p self
-  p Time.class_methods
+  p Time.methods
   # time = Time.new
   # if time.hour > 11
   #   resp.write "Good afternoon.\n"
